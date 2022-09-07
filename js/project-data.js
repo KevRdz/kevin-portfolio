a30c673c5c1e8fdd2b6ca7e774b1295092f6299f
Kevin <kevin.rodriguez678@gmail.com>
2022-09-07 14:49:51 -0400

    add project files to display my projects in containers using bootstrap

---
+++ js/project-data.js
@@ -0,0 +1,34 @@
+let projectData = [
+  {
+    title: 'tic-tac-toe',
+    deployment: 'https://unclehagi-ttt.netlify.app/',
+    image: '/assets/images/ttt.png',
+    github: 'https://github.com/UncleHagi/ttt-weekend',
+    description: 'Fun game to play with friends and family'
+  },
+  {
+    title: 'Fish Tracker',
+    deployment: "https://fish-tracker.herokuapp.com/",
+    image: '/assets/images/Fish-tracker.png',
+    github: 'https://github.com/UncleHagi/fish-tracker',
+    description: "A site created to help users track fishes."
+  },
+  {
+    title: 'Newt',
+    deployment: "https://newt-activity.netlify.app/",
+    image: '/assets/images/newt.png',
+    github: 'https://github.com/UncleHagi/newt-front-end.git',
+    description: "A fun webapp designed to give activity ideas to users."
+  },
+  // {
+  //   title: 'Hoarder of Cards',
+  //   deployment: "https://hoarder-of-cards.herokuapp.com/",
+  //   image: '/assets/images/hoarder-of-cards.png',
+  //   github: 'https://github.com/UncleHagi/hoarder-of-cards.git',
+  //   description: "Useful webapp to log and keep track of card collections."
+  // },
+]
+
+export{
+  projectData
+}
